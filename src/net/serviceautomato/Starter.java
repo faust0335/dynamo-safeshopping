d018af3bd8daa88eaa031982635acadb73086dcc
Chen, Yiqun <feedingfranzy@gmail.com>
2015-06-24 16:18:24 +0200

    [Starter] Fork a new thread for icap server

--- src/net/serviceautomato/Starter.java
+++ src/net/serviceautomato/Starter.java
@@ -3,9 +3,20 @@ package net.serviceautomato;
 import icap.IcapServer;
 
 public class Starter {
+	private static Thread icapInstance = new Thread() {
+		@Override
+		public void run() {
+			IcapServer.main(null);
+		}
+	};
 
 	public static void main(String[] args) {
-		IcapServer.main(null);
+		icapInstance.start();
+		try {
+			icapInstance.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
 }
