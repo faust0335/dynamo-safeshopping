66f5122f9ac736c9871873a528004281592f07f7
Chen, Yiqun <feedingfranzy@gmail.com>
2015-06-24 13:59:09 +0200

    [Starter] Create a starter stub

---
+++ src/net/serviceautomato/Starter.java
@@ -0,0 +1,13 @@
+package net.serviceautomato;
+
+import icap.IcapServer;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] icapArgs = new String[0];
+		IcapServer.main(icapArgs);
+	}
+
+}
