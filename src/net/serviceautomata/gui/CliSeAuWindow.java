f810b974b7c717a482101432311f461fa4566943
ue17apyt <napoelon@hotmail.com>
2015-07-15 16:47:55 +0200

    Add net.serviceautomata.gui for GUI manipulation

---
+++ src/net/serviceautomata/gui/CliSeAuWindow.java
@@ -0,0 +1,138 @@
+package net.serviceautomata.gui;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * 
+ * This class initiates a graphical user interface to manipulate the CliSeAu nodes
+ * 
+ * @author Xu, Yinhua
+ *
+ */
+
+public class CliSeAuWindow {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				CliSeAuFrame frame = new CliSeAuFrame();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		});
+	}
+}
+
+/**
+ * This class establishes a window frame for manipulation
+ * 
+ * @author Xu, Yinhua
+ *
+ */
+class CliSeAuFrame extends JFrame {
+	
+	public static final int DEFAULT_FRAME_ABSCISSA = 450;
+	public static final int DEFAULT_FRAME_ORDINATE = 350;
+	public static final int DEFAULT_FRAME_WIDTH = 320;
+	public static final int DEFAULT_FRAME_HEIGHT = 200;
+	public static final int DEFAULT_HORIZONTAL_GAP = 10;
+	public static final int DEFAULT_VERTICAL_GAP = 10;
+	
+	private JPanel panel;
+	private JLabel addressLabel;
+	private JLabel identifierLabel;
+	private JTextField displayAddress;
+	private JTextField displayIdentifier;
+	private JButton initCliSeAu;
+	private JButton addCliSeAu;
+	
+	public CliSeAuFrame(){
+		setTitle("Add CliSeAu Node");
+		setBounds(DEFAULT_FRAME_ABSCISSA, DEFAULT_FRAME_ORDINATE,
+				DEFAULT_FRAME_WIDTH, DEFAULT_FRAME_HEIGHT);
+		setLayout(new FlowLayout(FlowLayout.CENTER,
+				DEFAULT_HORIZONTAL_GAP, DEFAULT_VERTICAL_GAP));
+		CliSeAuPanel panel = new CliSeAuPanel();
+		add(panel);
+		pack();
+	}
+	
+	/**
+	 * A panel with text fields of both node address and node identifier, and 
+	 * the buttons to click to initialize a CliSeAu service and add CliSeAu nodes
+	 * 
+	 * @author Xu, Yinhua
+	 *
+	 */
+	class CliSeAuPanel extends JPanel {	
+		
+		public CliSeAuPanel() {
+					
+			setLayout(new BorderLayout());
+			
+			panel = new JPanel();
+			panel.setLayout(new GridLayout(3, 2,
+					DEFAULT_HORIZONTAL_GAP, DEFAULT_VERTICAL_GAP));
+			
+			addressLabel = new JLabel("NODE ADDRESS");
+			panel.add(addressLabel);
+			displayAddress = new JTextField("0.0.0.0:0");
+			panel.add(displayAddress);
+			
+			identifierLabel = new JLabel("NODE IDENTIFIER (INTEGER)");
+			panel.add(identifierLabel);
+			displayIdentifier = new JTextField("0");
+			panel.add(displayIdentifier);
+			
+			initCliSeAu = new JButton("INITIALIZE SERVICE");
+			ActionListener initListener = new InitCliSeAuAction();
+			initCliSeAu.addActionListener(initListener);
+			panel.add(initCliSeAu);
+			
+			addCliSeAu = new JButton("ADD CLISEAU NODE");
+			ActionListener addListener = new AddCliSeAuAction();
+			addCliSeAu.addActionListener(addListener);
+			panel.add(addCliSeAu);
+			
+			add(panel, BorderLayout.CENTER);
+		}
+		
+		/**
+		 * The action starts the CliSeAu service with an ICAP server and one
+		 * CliSeAu node
+		 * 
+		 * @author Chen, Yiqun
+		 *
+		 */
+		private class InitCliSeAuAction implements ActionListener {
+			
+			public void actionPerformed(ActionEvent event) {
+				// TODO
+			}
+		}
+		
+		/**
+		 * The action starts one more CliSeAu service on the basis of an available
+		 * ICAP server and the other existing CliSeAu node(s).
+		 * 
+		 * @author Chen, Yiqun
+		 *
+		 */
+		private class AddCliSeAuAction implements ActionListener {
+			
+			public void actionPerformed(ActionEvent event) {
+				// TODO
+			}
+		}
+	}
+}
