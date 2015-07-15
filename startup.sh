a927dfe1ba1eeef2bab363fd33f7ecb092a516ca
Chen, Yiqun <feedingfranzy@gmail.com>
2015-07-15 15:22:20 +0200

    [PATCH]Test script for new starter

--- startup.sh
+++ startup.sh
@@ -4,14 +4,14 @@
 java -jar SafeShopping.jar &
 
 # Start CliSeAu instance only
-java -jar SafeShopping.jar 7 &
-java -jar SafeShopping.jar 8 &
-java -jar SafeShopping.jar 29 &
-java -jar SafeShopping.jar 33 &
-java -jar SafeShopping.jar 37 &
-java -jar SafeShopping.jar 48 &
-java -jar SafeShopping.jar 51 &
-java -jar SafeShopping.jar 60 &
-java -jar SafeShopping.jar 63 &
+java -jar SafeShopping.jar localhost 7 &
+java -jar SafeShopping.jar localhost 8 &
+java -jar SafeShopping.jar localhost 29 &
+java -jar SafeShopping.jar localhost 33 &
+java -jar SafeShopping.jar localhost 37 &
+java -jar SafeShopping.jar localhost 48 &
+java -jar SafeShopping.jar localhost 51 &
+java -jar SafeShopping.jar localhost 60 &
+java -jar SafeShopping.jar localhost 63 &
 
 echo 'System has been successfully initialized'
